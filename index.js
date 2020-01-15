410b43eaac93d0043f57bf07f23732ce4d1ca03e
Marty Mavis <marty.mavis@gmail.com>
2020-01-15 10:51:33 -0600

    action code

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const trelloUrl = core.getInput('trello-url');
-  console.log(`Checking for tickets: ${trelloUrl}!`);
+  const linkRegEx = core.getInput('link-regex');
+  console.log(`Checking for links: ${linkRegEx}!`);
   const time = (new Date()).toTimeString();
+  
   core.setOutput("time", time);
+  core.setOutput("link", linkRegEx);
+  core.setOutput("time", false);
+
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
