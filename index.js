2f87950af48aea56c221666d7fb6cdcd8a189bf5
Marty Mavis <marty.mavis@gmail.com>
2020-01-15 13:01:03 -0600

    test

--- index.js
+++ index.js
@@ -6,19 +6,13 @@ async function run() {
         // `who-to-greet` input defined in action metadata file
         const linkRegExInput = core.getInput('link-regex');
         const linkRegExp = new RegExp(linkRegExInput)
+
         console.log(`Checking for links: ${linkRegExInput}!`);
-        const time = (new Date()).toTimeString();
-        
-        core.setOutput("time", time);
-        core.setOutput("link", linkRegExp);
-        core.setOutput("time", false);
-      
+
+        const time = (new Date()).toTimeString();      
         const token = core.getInput('repo-token');
         const octokit = new github.GitHub(token);
         const payload = github.context.payload;
-
-        console.log('payload:');
-        console.log(payload);
 
         const prComments = await octokit.issues.listComments({
             owner: payload.organization.login,
@@ -28,12 +22,18 @@ async function run() {
       
         const commentsWithLinks = prComments.data.filter(d => linkRegExp.exec(d.body).length > 0).map(i => linkRegExp.exec(i.body)[0])
 
-        console.log('all:')
-        console.log(prComments);
+        console.log('all prComments:')
+        console.log(prComments.data.map(i => i.body));
+
         console.log('matches:')
         console.log(commentsWithLinks);
 
-      } catch (error) {
+        core.setOutput('msg', `Found ${commentsWithLinks.length} with matching links`)
+
+        if(commentsWithLinks.length === 0)
+        core.setFailed(`unable to find any comments matching link ${linkRegExInput}`)
+
+    } catch (error) {
           console.log(error);
           core.setFailed(error.message);
       }
