5a437880a33f35fe3aad2c8367120854efb5929a
Marty Mavis <marty.mavis@gmail.com>
2020-01-15 12:03:15 -0600

    test

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const linkRegEx = core.getInput('link-regex');
-  console.log(`Checking for links: ${linkRegEx}!`);
-  const time = (new Date()).toTimeString();
-  
-  core.setOutput("time", time);
-  core.setOutput("link", linkRegEx);
-  core.setOutput("time", false);
-
-  const payload = github.context.payload;
-  const token = core.getInput('repo-token');
-  const octokit = new github.GitHub(token);
-  const prComments = await octokit.issues.listComments(payload.issue())
-
-  console.log(prComments);
-} catch (error) {
-    console.log(error);
-    core.setFailed(error.message);
+async function run() {
+    try {
+        // `who-to-greet` input defined in action metadata file
+        const linkRegEx = core.getInput('link-regex');
+        console.log(`Checking for links: ${linkRegEx}!`);
+        const time = (new Date()).toTimeString();
+        
+        core.setOutput("time", time);
+        core.setOutput("link", linkRegEx);
+        core.setOutput("time", false);
+      
+        const payload = github.context.payload;
+        const token = core.getInput('repo-token');
+        const octokit = new github.GitHub(token);
+        const prComments = await octokit.issues.listComments(payload.issue())
+      
+        console.log(prComments);
+      } catch (error) {
+          console.log(error);
+          core.setFailed(error.message);
+      }
 }
+
+run();
